860e1d3c82ff68b26544f755f16f1e26497326ea
joy <wendokingoo@gmail.com>
2019-01-21 14:00:05 +0300

    imports added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { UserService} from './services/user.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
