b11a89ef284861af470b35646811d6f271d89c06
joy <wendokingoo@gmail.com>
2019-01-21 14:05:59 +0300

    user service modified

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import{Http,Headers} from '@angular/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
+export class UserService {private username:string;
 
-  constructor() { }
+    constructor( private http:Http) {
+    console.log('service now ready');
+  this.username ='joyjoie';
+}
+getProfileInfo(){
+  return this.http.get("https://api.github.com/users/"+ this.username +"?access_token =d1a4b5149175e44c92d4b29f6fab41c4df850acb")
+  .pipe(map(res =>res.json()));
 }
