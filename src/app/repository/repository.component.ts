0a3788e3aeec1fd79af0ace9c297e78b93040fa2
joy <wendokingoo@gmail.com>
2019-01-22 17:07:35 +0300

    repository ts modified

--- src/app/repository/repository.component.ts
+++ src/app/repository/repository.component.ts
@@ -1,13 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ProfileService } from '../services/profile.service';
+import { environment} from '../../environments/environment';
+import { FormsModule} from '@angular/forms';
 @Component({
   selector: 'app-repository',
   templateUrl: './repository.component.html',
   styleUrls: ['./repository.component.css']
 })
 export class RepositoryComponent implements OnInit {
+  profile: any[];
+  repos: any[];
+  username:string;
+  constructor(private profileService: ProfileService) {
 
-  constructor() { }
+    }
+    findProfile(){
+      this.profileService.updateProfile(this.username);
+      this.profileService.getProfileInfo().subscribe(profile => {
+        console.log(profile);
+        this.profile = profile;
+      });
+      this.profileService.getProfileRepos().subscribe(repos => {
+        console.log(repos);
+        this.repos = repos;
+      });
+    }
+
 
   ngOnInit() {
   }
